cdb3e9fb08e9aa73ee47573663af40832fe59201
Ishwar <ishwargowda100@gmail.com>
2023-12-31 18:24:26 +0530

    feat: remove complex operands

--- compiler.py
+++ compiler.py
@@ -1,82 +1,88 @@
 import ast
 from ast import *
+from enum import EnumCheck
 from utils import *
+import utils
 from x86_ast import *
 import os
-from typing import List, Tuple, Set, Dict
+from typing import List, Sequence, Tuple, Set, Dict
 
 Binding = Tuple[Name, expr]
-Temporaries = List[Binding]
+Temporaries = Sequence[Binding]
 
 
 class Compiler:
-
     ############################################################################
     # Remove Complex Operands
     ############################################################################
 
-    def rco_exp(self, e: expr, need_atomic : bool) -> Tuple[expr, Temporaries]:
+    def rco_exp(self, e: ast.expr, need_atomic: bool) -> Tuple[ast.expr, Temporaries]:
         match e:
-          case  Constant(n):
-            return Constant(n), []
-          case Call(Name('input_int'),[]):
-            if need_atomic:
-              tmp = Name(generate_name('tmp'))
-              return tmp, [(tmp, Call(Name('input_int'),[]))]
-            else:
-              return Call(Name('input_int'),[]), []
-          case UnaryOp(USub(), exp):
-             (atm, temps) = self.rco_exp(exp, True)
-             usub = UnaryOp(USub(), atm)
-             if need_atomic:
-               tmp = Name(generate_name('tmp'))
-               return tmp, temps + [(tmp, usub)]
-             else:
-               return usub, temps
-          case BinOp(exp1, Add(), exp2):
-             (atm1, temps1) = self.rco_exp(exp1, True)
-             (atm2, temps2) = self.rco_exp(exp2, True)
-             add = BinOp(atm1, Add(), atm2)
-             if need_atomic:
-               tmp = Name(generate_name('tmp'))
-               return tmp, temps1 + temps2 + [(tmp, add)]
-             else:
-               return add, temps1 + temps2
-          case BinOp(exp1, Sub(), exp2):
-             (atm1, temps1) = self.rco_exp(exp1, True)
-             (atm2, temps2) = self.rco_exp(exp2, True)
-             sub = BinOp(atm1, Sub(), atm2)
-             if need_atomic:
-               tmp = Name(generate_name('tmp'))
-               return tmp, temps1 + temps2 + [(tmp, sub)]
-             else:
-               return sub, temps1 + temps2
-          case Name(var):
-             return Name(var), []
-          case _:
-            raise Exception('rco_exp unexpected ' + repr(e))
+            case ast.Constant(v):
+                return ast.Constant(v), []
+            case ast.Name(var):
+                return ast.Name(var), []
+            case ast.Call(ast.Name("input_int"), []):
+                if need_atomic:
+                    tmp = utils.generate_name("tmp")
+                    return (
+                        ast.Name(tmp),
+                        [(ast.Name(tmp), ast.Call(ast.Name("input_int"), []))],
+                    )
+                return (ast.Call(ast.Name("input_int"), []), [])
+            case ast.UnaryOp(ast.USub(), exp):
+                (atm, temps) = self.rco_exp(exp, True)
+                if need_atomic:
+                    tmp = utils.generate_name("tmp")
+                    return (
+                        ast.Name(tmp),
+                        [*temps, (ast.Name(tmp), ast.UnaryOp(ast.USub(), atm))],
+                    )
+                return (ast.UnaryOp(ast.USub(), atm), temps)
+            case ast.BinOp(exp1, op, exp2):
+                (atm1, temps1) = self.rco_exp(exp1, True)
+                (atm2, temps2) = self.rco_exp(exp2, True)
+                if need_atomic:
+                    tmp = utils.generate_name("tmp")
+                    return (
+                        ast.Name(tmp),
+                        [
+                            *temps1,
+                            *temps2,
+                            (ast.Name(tmp), ast.BinOp(atm1, op, atm2)),
+                        ],
+                    )
 
-    def rco_stmt(self, s: stmt) -> List[stmt]:
-      match s:
-        case Expr(Call(Name('print'), [exp])):
-           (atm, temps) = self.rco_exp(exp, True)
-           return [Assign([var], init) for (var,init) in temps] + [Expr(Call(Name('print'), [atm]))]
-        case Expr(exp) :
-           (atm, temps) = self.rco_exp(exp, False)
-           return [Assign([var], init) for (var,init) in temps]
-        case Assign([Name(var)], exp):
-           (atm, temps) = self.rco_exp(exp, False)
-           return [Assign([x], init) for (x,init) in temps] + [Assign([Name(var)], atm)]
-        case _:
-          raise Exception('rco_stmt not implemented')
+                return (ast.BinOp(atm1, op, atm2), [*temps1, *temps2])
+            case _:
+                raise Exception(f"rco_exp: unexpected value: {e}")
 
-    def remove_complex_operands(self, p: Module) -> Module:
+    def rco_stmt(self, s: ast.stmt) -> Sequence[ast.stmt]:
+        match s:
+            case ast.Expr(ast.Call(ast.Name("print"), [exp])):
+                (atm, temps) = self.rco_exp(exp, True)
+                return [ast.Assign([var], value) for (var, value) in temps] + [
+                    ast.Expr(ast.Call(ast.Name("print"), [atm]))
+                ]
+            case ast.Expr(expr):
+                (atm, temps) = self.rco_exp(expr, False)
+                return [ast.Assign([var], value) for (var, value) in temps] + [
+                    ast.Expr(atm)
+                ]
+            case ast.Assign([ast.Name(var)], exp):
+                (atm, temps) = self.rco_exp(exp, True)
+                return [ast.Assign([var], value) for (var, value) in temps] + [
+                    ast.Assign([ast.Name(var)], atm)
+                ]
+            case _:
+                raise Exception(f"rco_stmt: unexpected stmt: {s}")
+
+    def remove_complex_operands(self, p: ast.Module) -> ast.Module:
         match p:
-          case Module(ss):
-            sss = [self.rco_stmt(s) for s in ss]
-            return Module(sum(sss, []))
-        raise Exception('remove_complex_operands not implemented')
-        
+            case ast.Module(ss):
+                sss = [stmt for s in ss for stmt in self.rco_stmt(s)]
+                return ast.Module(sss)
+        raise Exception("remove_complex_operands not implemented")
 
     ############################################################################
     # Select Instructions
@@ -84,15 +90,15 @@ class Compiler:
 
     def select_arg(self, e: expr) -> arg:
         # YOUR CODE HERE
-        pass        
+        pass
 
     def select_stmt(self, s: stmt) -> List[instr]:
         # YOUR CODE HERE
-        pass        
+        pass
 
     def select_instructions(self, p: Module) -> X86Program:
         # YOUR CODE HERE
-        pass        
+        pass
 
     ############################################################################
     # Assign Homes
@@ -100,16 +106,15 @@ class Compiler:
 
     def assign_homes_arg(self, a: arg, home: Dict[Variable, arg]) -> arg:
         # YOUR CODE HERE
-        pass        
+        pass
 
-    def assign_homes_instr(self, i: instr,
-                           home: Dict[Variable, arg]) -> instr:
+    def assign_homes_instr(self, i: instr, home: Dict[Variable, arg]) -> instr:
         # YOUR CODE HERE
-        pass        
+        pass
 
     def assign_homes(self, p: X86Program) -> X86Program:
         # YOUR CODE HERE
-        pass        
+        pass
 
     ############################################################################
     # Patch Instructions
@@ -117,11 +122,11 @@ class Compiler:
 
     def patch_instr(self, i: instr) -> List[instr]:
         # YOUR CODE HERE
-        pass        
+        pass
 
     def patch_instructions(self, p: X86Program) -> X86Program:
         # YOUR CODE HERE
-        pass        
+        pass
 
     ############################################################################
     # Prelude & Conclusion
@@ -129,5 +134,4 @@ class Compiler:
 
     def prelude_and_conclusion(self, p: X86Program) -> X86Program:
         # YOUR CODE HERE
-        pass        
-
+        pass

027ca448517a98ce09fe322696264be28fdfafce
Ishwar <ishwargowda100@gmail.com>
2024-02-27 20:10:07 +0530

    feat: implement `uncover_live()`
    Co-Authored-By: Samyak S Sarnayak <samyak201@gmail.com>

--- compiler_register_allocator.py
+++ compiler_register_allocator.py
@@ -4,26 +4,79 @@ from typing import List, Tuple, Set, Dict
 from ast import *
 from x86_ast import *
 from typing import Set, Dict, Tuple
+import x86_ast
 
 # Skeleton code for the chapter on Register Allocation
 
-class Compiler(compiler.Compiler):
 
+class Compiler(compiler.Compiler):
     ###########################################################################
     # Uncover Live
     ###########################################################################
 
-    def read_vars(self, i: instr) -> Set[location]:
-        # YOUR CODE HERE
-        pass
+    @staticmethod
+    def _location_set(arg: x86_ast.arg) -> set[x86_ast.location]:
+        if isinstance(arg, (x86_ast.Reg, x86_ast.Variable)):
+            return {arg}
 
-    def write_vars(self, i: instr) -> Set[location]:
-        # YOUR CODE HERE
-        pass
+        return set()
 
-    def uncover_live(self, p: X86Program) -> Dict[instr, Set[location]]:
-        # YOUR CODE HERE
-        pass
+    def read_vars(self, i: x86_ast.instr) -> Set[x86_ast.location]:
+        match i:
+            case x86_ast.Instr("addq" | "subq", [arg_0, arg_1]):
+                return self._location_set(arg_0) | self._location_set(arg_1)
+            case x86_ast.Instr("movq", [arg_0, _]):
+                return self._location_set(arg_0)
+            case x86_ast.Instr("negq" | "pushq", [arg_0]):
+                return self._location_set(arg_0)
+            case x86_ast.Callq(_, arg_num):
+                arg_regs = [
+                    x86_ast.Reg("rdi"),
+                    x86_ast.Reg("rsi"),
+                    x86_ast.Reg("rdx"),
+                    x86_ast.Reg("rcx"),
+                    x86_ast.Reg("r8"),
+                    x86_ast.Reg("r9"),
+                ]
+                return set(arg_regs[:arg_num])
+        return set()
+
+    def write_vars(self, i: x86_ast.instr) -> Set[x86_ast.location]:
+        match i:
+            case x86_ast.Instr("addq" | "subq" | "movq", [_, arg_1]):
+                return self._location_set(arg_1)
+            case x86_ast.Instr("negq" | "popq", [arg_0]):
+                return self._location_set(arg_0)
+            case x86_ast.Callq(_, _):
+                return {
+                    x86_ast.Reg("rax"),
+                    x86_ast.Reg("rcx"),
+                    x86_ast.Reg("rdx"),
+                    x86_ast.Reg("rsi"),
+                    x86_ast.Reg("rdi"),
+                    x86_ast.Reg("r8"),
+                    x86_ast.Reg("r9"),
+                    x86_ast.Reg("r10"),
+                    x86_ast.Reg("r11"),
+                }
+
+        return set()
+
+    def uncover_live(
+        self, p: x86_ast.X86Program
+    ) -> dict[x86_ast.instr, Set[x86_ast.location]]:
+        assert isinstance(p.body, list)
+
+        live_after_set: dict[x86_ast.instr, Set[x86_ast.location]] = {p.body[-1]: set()}
+        next_after_set = set()
+        for instr, next_instr in zip(reversed(p.body[:-1]), reversed(p.body)):
+            next_bofore_set = (
+                next_after_set - self.write_vars(next_instr)
+            ) | self.read_vars(next_instr)
+            live_after_set[instr] = next_bofore_set
+            next_after_set = next_bofore_set
+
+        return live_after_set
 
     ############################################################################
     # Build Interference

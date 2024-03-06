f042e24ba0eaaa285b8006b7c36fe9186cf4dad0
Samyak S Sarnayak <samyak201@gmail.com>
2024-03-06 21:01:31 +0530

    test: add test case for stack spill
    Co-Authored-By: Ishwar <ishwargowda100@gmail.com>

---
+++ tests/var/stack-spill.py
@@ -0,0 +1,40 @@
+a01 = 1
+a02 = 2
+a03 = 3
+a04 = 4
+a05 = 5
+a06 = 6
+a07 = 7
+a08 = 8
+a09 = 9
+a10 = 10
+a11 = 11
+a12 = 12
+a13 = 13
+a14 = 14
+a15 = 15
+a16 = 16
+a17 = 17
+a18 = 18
+a19 = 19
+a20 = 20
+print(a20)
+print(a19)
+print(a18)
+print(a17)
+print(a16)
+print(a15)
+print(a14)
+print(a13)
+print(a12)
+print(a11)
+print(a10)
+print(a09)
+print(a08)
+print(a07)
+print(a06)
+print(a05)
+print(a04)
+print(a03)
+print(a02)
+print(a01)
